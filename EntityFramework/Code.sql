b571c45a1e07291abc14df541c10e3117ca138f3
NoytroO <montes.yannick@gmail.com>
2014-12-24 14:19:03 +0100

    Finitions

--- EntityFramework/Code.sql
+++ EntityFramework/Code.sql
@@ -5,8 +5,6 @@ GO
 IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
 GO
 
---INSERT INTO EtatSet(Code, Libelle) VALUES ('007','Créée');
---INSERT INTO EtatSet(Code, Libelle) VALUES ('008','En cours');
---INSERT INTO EtatSet(Code, Libelle) VALUES ('009','Terminée');
-
---SELECT * FROM EtatSet
+INSERT INTO EtatSet(Code, Libelle) VALUES ('007','Créée');
+INSERT INTO EtatSet(Code, Libelle) VALUES ('008','En cours');
+INSERT INTO EtatSet(Code, Libelle) VALUES ('009','Terminée');
